2543c986178848ba70cf5bac3b5209580e3af937
kflemin <katherine.fleming@nrel.gov>
2022-05-11 23:30:43 -0600

    lab space -> distribution connections

--- src/app/laboratories/lab-space/exposure-control-devices/exposure-control-devices.component.ts
+++ src/app/laboratories/lab-space/exposure-control-devices/exposure-control-devices.component.ts
@@ -9,6 +9,7 @@ export class ExposureControlDevicesComponent {
 
   constructor(private sharedService: SharedService) {
     sharedService.setTitle(this.title);
+    sharedService.setActiveSelectors('hoodoutline');
   }
 
 }
