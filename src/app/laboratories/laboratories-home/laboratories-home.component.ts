d5fd196c72c29c9ee995a6a2435248bf5116319b
Alex Swindler <Alex.Swindler@nrel.gov>
2022-11-21 11:12:57 -0700

    Restored NREL template header and footer - Fixed unsubscription bugs - Added eslint - Fixed security vulnerabilities - Removed unused images, optimized images - Fixed several svg zoom bugs - Fixed svg formatting - Fixed mixed tabs/spaces - Removed console logging

--- src/app/laboratories/laboratories-home/laboratories-home.component.ts
+++ src/app/laboratories/laboratories-home/laboratories-home.component.ts
@@ -12,17 +12,18 @@ export class LaboratoriesHomeComponent {
     sharedService.setTitle(this.title);
 
     const url_segments = this.router.url.split('/');
-    const last = url_segments[url_segments.length -1];
-    if (last == 'central-plant') {
-    	sharedService.setActiveSelectors('cp-outline');
-    } else if (last == 'lab-space') {
-    	sharedService.setActiveSelectors('lab_outline');
-    } else if (last == 'lab-exhaust-devices') {
-    	sharedService.setActiveSelectors('exhaustoutline');
-    } else if (last == 'distribution') {
-    	sharedService.setActiveSelectors('disoutline');
+    const last = url_segments[url_segments.length - 1];
+    if (last === 'central-plant') {
+      sharedService.setActiveSelectors('cp-outline');
+    } else if (last === 'lab-space') {
+      sharedService.setActiveSelectors('lab_outline');
+    } else if (last === 'lab-exhaust-devices') {
+      sharedService.setActiveSelectors('exhaustoutline');
+    } else if (last === 'distribution') {
+      sharedService.setActiveSelectors('disoutline');
+    } else {
+      sharedService.setActiveSelectors();
     }
-
   }
 
 }
--- src/app/shared/directives/external-link.directive.ts
+++ src/app/shared/directives/external-link.directive.ts
@@ -1,27 +1,31 @@
-import { Directive, ElementRef, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, Input, OnChanges } from '@angular/core';
 
 @Directive({
+  // eslint-disable-next-line @angular-eslint/directive-selector
   selector: 'a[href]'
 })
-export class ExternalLinkDirective {
-  @HostBinding('attr.rel') relAttr?: string;
-  @HostBinding('attr.target') targetAttr?: string;
-  @Input() href = '';
-
-  constructor(private elementRef: ElementRef) {
-  }
+export class ExternalLinkDirective implements OnChanges {
+  @HostBinding('attr.href') hrefAttr: string | null = null;
+  @HostBinding('attr.rel') relAttr: string | null = null;
+  @HostBinding('attr.target') targetAttr: string | null = null;
+  @Input() href: string | undefined;
 
   ngOnChanges() {
-    if (this.isExternal()) {
-      this.relAttr = 'noopener';
-      this.targetAttr = '_blank';
-    } else {
-      this.relAttr = undefined;
-      this.targetAttr = undefined;
+    if (this.href) {
+      this.hrefAttr = this.href;
+
+      if (this.isLinkExternal(this.href)) {
+        this.relAttr = 'noopener';
+        this.targetAttr = '_blank';
+      } else {
+        this.relAttr = null;
+        this.targetAttr = null;
+      }
     }
   }
 
-  private isExternal() {
-    return this.elementRef.nativeElement.origin !== location.origin;
+  private isLinkExternal(href: string | null) {
+    if (!href || href.startsWith('mailto:')) return false;
+    return new URL(document.baseURI).origin !== new URL(href, document.baseURI).origin;
   }
 }
